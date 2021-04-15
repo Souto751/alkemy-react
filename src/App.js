3f5b679a1681bb487b27df55b4811052d75522b3
Souto751 <agus.souto75@gmail.com>
2021-04-15 16:58:34 -0300

    Add Router and Routes on App.js Add the routes to each page with their corresponding components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import './App.css';
+//------------------//
+//------Import------//
+//------------------//
 
-function App() {
+//------React------//
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+//------Components------//
+import Home from './components/home/Home';
+import Character from './components/hero-info/HeroPage';
+import Login from './components/login/Login';
+import Register from './components/login/Register';
+import PswdRecovery from './components/login/PswdRecovery';
+import PswdChange from './components/login/PswdChange';
+
+//------------------//
+//------Export------//
+//------------------//
+
+//------Component------//
+export default function App() {
   return (
-    <div className="App">
-      
-    
-    </div>
+    <Router>
+      <div className="App">
+        
+        <Route exact path="/" component={Home} />
+
+        <Route path="/info/character/" component={Character} />
+
+        <Route exact path="/login" component={Login} />
+
+        <Route exact path="/register" component={Register} />
+
+        <Route exact path="/reset-password" component={PswdRecovery} />
+
+        <Route exact path="/reset-password/enter-new-password" component={PswdChange} />
+
+      </div>
+    </Router>
   );
 }
-
-export default App;
