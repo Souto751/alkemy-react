1cb60eeb0a014c8d2a0f6f8fe1445e34ea8c5b56
Souto751 <agus.souto75@gmail.com>
2021-04-20 15:47:31 -0300

    Add Register Screen It is not functional yet.

--- src/components/login/Register.jsx
+++ src/components/login/Register.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import '../../style/register.css';
 
 export default function Register() {
     return (
-        <div>
-            
+        <div className="register">
+            <h1 className="register-title">Register</h1>
+            <form className="register-form">
+                <h3 className="input-name">Email</h3>
+                <input type="email" className="register-input" placeholder="Enter your email..." />
+                <h3 className="input-name">Confirm Email</h3>
+                <input type="email" className="register-input" placeholder="Confirm your email..." />
+                <h3 className="input-name">Password</h3>
+                <input type="password" className="register-input" placeholder="Enter your password..." />
+                <h3 className="input-name">ConfirmPassword</h3>
+                <input type="password" className="register-input" placeholder="Confirm your password..." />
+                <button className="btn btn-primary">Register</button>
+            </form>
+            <p className="to-login-line">Already have an account? <Link className="to-login" to="/login">Login</Link></p>
         </div>
     )
 }
